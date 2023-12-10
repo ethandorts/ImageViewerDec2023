495c006c9ce54d765babfd266308622f064e4cf3
Ethan Doherty <ethandorts2002@gmail.com>
2023-12-10 15:58:06 +0000

    Fucntionality working

--- app/src/main/java/com/example/imageviewerdec2023/MainActivity.java
+++ app/src/main/java/com/example/imageviewerdec2023/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.imageviewerdec2023;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ImageView image;
@@ -39,15 +48,61 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 switch(which) {
                     case 0:
-
+                    getImageFromGallery();
                         break;
                     case 1:
-
+                    captureImagesFromCamera();
                         break;
                 }
             }
         });
         builder.show();
     }
+
+    private void captureImagesFromCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            cameraResultLauncher.launch(cameraIntent);
+        } else {
+            Toast.makeText(MainActivity.this, "camera isn't working", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap bitmap = (Bitmap) extras.get("data");
+                        image.setImageBitmap(bitmap);
+                    }
+                }
+            }
+    );
+
+    private void getImageFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        if(galleryIntent.resolveActivity(getPackageManager()) != null) {
+            galleryResultLauncher.launch(galleryIntent);
+        } else {
+            Toast.makeText(MainActivity.this, "gallery is not opening", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    ActivityResultLauncher<Intent> galleryResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Uri selectedImage = result.getData().getData();
+                        image.setImageURI(selectedImage);
+                    }
+                }
+            }
+    );
 
 }
